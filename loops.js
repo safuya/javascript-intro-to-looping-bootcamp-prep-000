36a7fec8c7eb4a08e5eca71b3cc283f1ecf3106a
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-13 08:52:58 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,6 +18,10 @@ function whileLoop(number) {
   return 'done'
 }
 
-function doWhileLoop(array) {
-  
+function maybeTrue() {
+  return Math.random() >= 0.5
 }
+
+function doWhileLoop(array) {
+  array.pop()
+} while (maybeTrue() || array.length < 1);
